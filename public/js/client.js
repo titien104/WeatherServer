05af3e069c8c26bdf4efee1ff11adecc21a6f162
Thien Nguyen <titien104@yahoo.com>
2020-01-28 17:27:42 -0600

    modify css and client

--- public/js/client.js
+++ public/js/client.js
@@ -2,21 +2,6 @@ console.log('client is starting')
 
 const failURL = 'http://localhost:3000/weather?address=!'
 const testURL = 'http://puzzle.mead.io/puzzle'
-// fetch(testURL).then((res)=> {
-//     res.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-// fetch(appURL).then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error)
-//         } else {
-//             console.log(data)
-//         }
-//     })
-// })
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
@@ -27,7 +12,7 @@ weatherForm.addEventListener("submit", e => {
     const location = search.value
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    const appURL = 'http://localhost:3000/weather?address='+location
+    const appURL ='/weather?address='+location
     //console.log(appURL)
     fetch(appURL).then((response) => {
         response.json().then((data) => {
